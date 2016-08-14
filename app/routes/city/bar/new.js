5415dbe7a9d5861d670125942edbf3b602545711
HarrisonA <radiov@gmail.com>
2016-08-14 00:49:30 -0700

    new bars can be added to a city. Eventually this will be an admin only privilidge

--- app/routes/city/bar/new.js
+++ app/routes/city/bar/new.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return {};
+  },
+
+  renderTemplate() {
+    this.render('city.bar.new', { into: 'application' });
+  },
+
+  actions: {
+    save() {
+      const city = this.modelFor('city');
+      const newBar = this.get('store').createRecord('bar', this.currentModel);
+      newBar.set('city', city);
+      newBar.save().then(() => {
+        this.transitionTo('city', city);
+      });
+    },
+
+    cancel() {
+      this.transitionTo('city', this.modelFor('city'));
+    },
+  },
 });
