4bbbb58dbf8055e5792bcdd129613318aa5998de
HarrisonA <radiov@gmail.com>
2016-08-23 11:41:48 -0700

    add empty life cycle hooks for activities-list component

--- app/components/activities-list.js
+++ app/components/activities-list.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  init() {
+    this._super(...arguments);
+
+    // console.log("activity list componenet instantiated");
+  },
+
+  willInsertElement() {
+    // console.log("before activity list componenet is inserted");
+  },
+
+  didInsertElement() {
+    // console.log("after activity list componenet is inserted");
+  },
+
+  willDestroyElement() {
+    // console.log("activity list is about to be removed from DOM");
+  },
 });
