9e9d9dee0f5a44827179a513c8ac72d7b19d646f
HarrisonA <radiov@gmail.com>
2016-08-24 09:37:28 -0700

    added Oakland and Oakland bars routes.  Other cities and activities currently not tested

--- tests/acceptance/app-routing-test.js
+++ tests/acceptance/app-routing-test.js
@@ -15,10 +15,51 @@ test('index should reroute to cities list', function (assert) {
   });
 });
 
-// test('should link to contact information.', function (assert) {
-//   visit('/');
-//   click('a:contains("Contact")');
-//   andThen(function () {
-//     assert.equal(currentURL(), '/contact', "should navigate to contact");
-//   });
-// });
+test('should link to random activity page.', function (assert) {
+  visit('/');
+  click('a:contains("random")');
+  andThen(function () {
+    assert.equal(currentURL(), '/random', "should navigate to random activity page");
+  });
+});
+
+// *********
+// The following tests are just checking one city and just checking "bars"
+// The other cities (and activities) are not being checked yet
+// *******
+
+// NOTE this test will fail if Oakland city id changes
+test('should link to Oakland activity page.', function (assert) {
+  visit('/');
+  click('a:contains("Oakland")');
+  andThen(function () {
+    assert.equal(currentURL(), '/cities/1', "should navigate to Oakland activity page");
+  });
+});
+
+// NOTE this test will fail if Oakland city id changes
+test('should link to Oakland bars page.', function (assert) {
+  visit('/cities/1');
+  click('a:contains("Bars")');
+  andThen(function () {
+    assert.equal(currentURL(), '/cities/1/bars', "should navigate to Oakland bars page");
+  });
+});
+
+// NOTE this test will fail if Oakland city id changes
+test('should link back to cities list.', function (assert) {
+  visit('/cities/1');
+  click('a:contains("Back to")');
+  andThen(function () {
+    assert.equal(currentURL(), '/cities', "should navigate to cities page");
+  });
+});
+
+// NOTE this test will fail if Oakland city id changes
+test("should link to 'Add a bar' page.", function (assert) {
+  visit('/cities/1/bars');
+  click('a:contains("Add a new bar")');
+  andThen(function () {
+    assert.equal(currentURL(), '/cities/1/bars/new', "should navigate to 'Add a bar' page");
+  });
+});
