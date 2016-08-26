260c1fb066f3152f7cf8ae3c399bdb14e903fa7d
HarrisonA <radiov@gmail.com>
2016-08-26 13:20:01 -0700

    added clarifying comments.  Added msg to display on existing but unused routes

--- app/routes/city/bar/new.js
+++ app/routes/city/bar/new.js
@@ -5,17 +5,25 @@ export default Ember.Route.extend({
     return {};
   },
 
+  // NOTE: Because bar/new is a *subroute* of city,
+  // renderTemplate is needed to display city/bar/new.hbs
   renderTemplate() {
     this.render('city.bar.new', { into: 'application' });
   },
 
   actions: {
     save() {
+
+      // assign the current city to a variable
       const city = this.modelFor('city');
+
+      // NOTE: this.currentModel is model containing the input values
+      // from form in the .hbs file
       const newBar = this.get('store').createRecord('bar', this.currentModel);
-      newBar.set('city', city);
+
+      newBar.set('city', city);  //assign the city to this new bar
       newBar.save().then(() => {
-        this.transitionTo('city', city);
+        this.transitionTo('city', city);  //return to the city page
       });
     },
 
