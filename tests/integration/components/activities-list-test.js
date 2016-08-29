9d2263c9cf9b8c6d731f2f944817038c3a8fa318
HarrisonA <radiov@gmail.com>
2016-08-29 14:36:21 -0700

    Added assert to test that component renders correctly

--- tests/integration/components/activities-list-test.js
+++ tests/integration/components/activities-list-test.js
@@ -1,24 +1,51 @@
+import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('activities-list', 'Integration | Component | activities list', {
-  integration: true
+moduleForComponent ('activities-list', 'Integration | Component | activities list', {
+  integration: true,
 });
 
-test('it renders', function(assert) {
+test('it renders', function (assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });"
 
-  this.render(hbs`{{activities-list}}`);
+  //this.render(hbs`{{activities-list}}`);
+  //assert.equal(this.$().text().trim(), '');
 
-  assert.equal(this.$().text().trim(), '');
+  //  Create a dummy model for the component
+  const dummyActivityObj = Ember.Object.extend({
+      name: "Solas Pub",
+      url: "http://www.madoakbar.com/",
+      description: "Outdoor/Indoor Bar",
+      why: "Chill, lots of space, cool crowd",
+      numofvisits:0,
+      photo:"http://static.wixstatic.com/media/cb3644_5f2ed69191e04daa8ef4feec40d245d1.jpg/v1/fill/w_348,h_463,al_c,q_90/cb3644_5f2ed69191e04daa8ef4feec40d245d1.jpg",
+      notes: "none",
+      hide:false,
+    }).create();
+
+  this.set('model', dummyActivityObj);
 
   // Template block usage:"
-  this.render(hbs`
-    {{#activities-list}}
-      template block text
-    {{/activities-list}}
-  `);
+  this.render(hbs`{{activities-list activity=model}}`);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$('strong').text(), "Solas Pub");
 });
+
+// Component template
+// <div>
+//   <strong>{{activity.name}}</strong>
+//   <br>
+//   <a href={{activity.url}}>{{activity.url}}</a>
+//   <br>
+//   <img src={{activity.photo}} alt="photo missing">
+//
+//   <p>About: {{activity.description}}</p>
+//
+//   <p>Why? {{activity.why}}</p>
+//
+//   <p>Visits: {{activity.numofvisits}}</p>
+//
+//   <p>Notes: {{activity.notes}}</p>
+// </div>
