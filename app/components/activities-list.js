31cf5843dbeda5dfb14c4459120474939d0e2766
Adesola Harrison <adesolah@netflix.com>
2016-09-22 08:48:33 -0700

    added comments to explain component behavior and functionality

--- app/components/activities-list.js
+++ app/components/activities-list.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  // somePropName: value,    // Set some property to be available in this component's template
+  // somePropName2: value,
+
+  // component's life cycle hooks that allow you to insert functionality at different stages
   init() {
     this._super(...arguments);
 
@@ -18,4 +22,10 @@ export default Ember.Component.extend({
   willDestroyElement() {
     // console.log("activity list is about to be removed from DOM");
   },
+
+  // actions: {
+  //   actionFunctionName(someInput){
+  //     action body goes here
+  //  },
+  // }
 });
